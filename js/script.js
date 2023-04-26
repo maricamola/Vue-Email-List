c5e1fdadb8b75db325e5dc1524391ae8600e2877
maricamola <maricamola98@libero.it>
2023-04-26 09:50:10 +0200

    printed 10 emails

--- js/script.js
+++ js/script.js
@@ -2,17 +2,36 @@ const {createApp} = Vue;
 
 createApp ({
   data(){
-    return{
 
+    return{
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      mails: [],
+      loading: true,
+      maxMail: 10,
+      limit: 10
+    }
+
+  },
+//Chiamata API
+  methods:{
+    getApi(){
+
+      this.loading = true;
+
+      axios.get(this.apiUrl)
+        .then( res => {
+        //in questa API mi serve la proprità response
+        this.mail = res.data.response
+        console.log(this.mails);
+        this.mails.push(res.data.response);
+        this.loading =false
+      })
     }
   },
-
+//Richiamo l'API al mounted
   mounted(){
-
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then( (risultato ) => {
-      console.log(risultato);
-
-    })
+    for(let i = 0; i < this.limit; i++)
+    this.getApi()
   }
+
 }).mount('#app')
